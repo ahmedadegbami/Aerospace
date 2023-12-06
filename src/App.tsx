3bcb8fd4cc40ac0cb749c135662b48bf17e5ac8e
ahmedadegbami <ahmed.adegbami@gmail.com>
2023-12-06 23:29:34 +0100

    get sensor data and visualize with barchart

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,95 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Bar } from "react-chartjs-2";
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
+
+interface SpectrumStatusData {
+  velocity: number;
+  altitude: number;
+  temperature: number;
+  statusMessage: string;
+  isAscending: boolean;
+  isActionRequired: boolean;
+}
 
 function App() {
+  const [spectrumStatus, setSpectrumStatus] = useState<SpectrumStatusData>();
+
+  const getSpectrumStatus = async () => {
+    try {
+      const response = await fetch(
+        "https://webfrontendassignment-isaraerospace.azurewebsites.net/api/SpectrumStatus"
+      );
+      if (!response.ok) {
+        const errorMessage = `Error: ${response.status} - ${response.statusText}`;
+        throw new Error(errorMessage);
+      }
+      const data = await response.json();
+      setSpectrumStatus(data);
+    } catch (error) {
+      console.log(error);
+      throw new Error("Something went wrong");
+    }
+  };
+
+  const data = {
+    labels: ["Velocity", "Altitude", "Temperature"],
+    datasets: [
+      {
+        label: "Spectrum Status",
+        data: [
+          `${spectrumStatus?.velocity}`,
+          `${spectrumStatus?.altitude}`,
+          `${spectrumStatus?.temperature}`,
+        ],
+        backgroundColor: "blue",
+        maxBarThickness: 80,
+        borderColor: "red",
+      },
+    ],
+  };
+
+  useEffect(() => {
+    getSpectrumStatus();
+
+    const intervalId = setInterval(() => {
+      getSpectrumStatus();
+    }, 30000); // it refresh every 30 secs
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "50%",
+      }}
+    >
+      <h2>Spectrum Data Visualization</h2>
+      {spectrumStatus && (
+        <>
+          <Bar
+            data={data}
+            style={{
+              border: "1px solid black",
+            }}
+          />
+          <div>
+            <h3>Details:</h3>
+            <p>Status: {`${spectrumStatus.statusMessage}`}</p>
+            <p>Is Ascending: {`${spectrumStatus.isAscending}`}</p>
+            <p>Is Action Required: {`${spectrumStatus.isActionRequired}`}</p>
+          </div>
+        </>
+      )}
+
+      <button onClick={() => getSpectrumStatus()}>Show Current Status</button>
     </div>
   );
 }
